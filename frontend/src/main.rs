eee1fc3eac1414d5eed79f3ee6a42ca86d4083d4
Dimitri <dimitri.timoz@orange.fr>
2024-01-13 19:35:51 +0100

    init prj

---
+++ frontend/src/main.rs
@@ -0,0 +1,15 @@
+use yew::prelude::*;
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+
+}
+
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <div>
+            <h2 class={"heading"}>{"Hello, World!"}</h2>
+        </div>
+    }
+}
